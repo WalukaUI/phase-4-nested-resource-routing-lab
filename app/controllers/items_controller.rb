f64be9e62f5a703bf690ad0cbfe518b4b4bafc8e
WalukaUI <cwaluka@yahoo.com>
2021-09-04 15:02:28 -0700

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
-
-  def index
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+def index
+  if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+  else
     items = Item.all
-    render json: items, include: :user
   end
+  render json: items, include: :user
+end
 
+def create
+  user = User.find(params[:user_id])
+  items = user.items.create(itemparams)
+  render json: items, status: 201
+end
+
+
+def show
+  item = find_item
+  render json: item
+end
+
+  private
+
+  def find_item
+    Item.find(params[:id])
+  end
+  def itemparams
+    params.permit(:name, :description, :price)
+  end
+  def not_found_response(i)
+    render json: { error: "#{i.model} not found" }, status: 404
+  end
 end
